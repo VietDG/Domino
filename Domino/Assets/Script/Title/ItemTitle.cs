3e6056c0a1d51d66597a5c4bdd86dff7ba0219fe
VietDG <97847061+VietDG@users.noreply.github.com>
2023-12-25 17:31:54 +0700

    Update

--- Domino/Assets/Script/Title/ItemTitle.cs
+++ Domino/Assets/Script/Title/ItemTitle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
@@ -10,14 +11,44 @@ public class ItemTitleData
     public ItemTitleData() { }
 
     public ItemTitleData(int id) { this.id = id; }
+
 }
 
 public class ItemTitle : MonoBehaviour
 {
     public ItemTitleData data;
 
+    public bool isMouseDown;
+
+    public Collider2D touchCollider2D;
+
     public void InitTitleData(ItemTitleData data)
     {
         this.data = data;
+    }
+
+    public void OnMouseDown()
+    {
+        isMouseDown = true;
+        TouchItem();
+    }
+
+    public void OnMouseUp()
+    {
+        if (isMouseDown)
+        {
+            isMouseDown = false;
+        }
+    }
+
+    public void SetTouch(bool isTouch)
+    {
+        touchCollider2D.enabled = isTouch;
+    }
+
+    public void TouchItem()
+    {
+        TitleManager.Instance.hold.AddItemToSlot(this);
+        TitleManager.Instance.items.Remove(this);
     }
 }
