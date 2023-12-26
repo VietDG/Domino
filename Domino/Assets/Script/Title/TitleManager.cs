76ac540bb6334fa03608accfb98a0ac74a4391cf
VietDG <97847061+VietDG@users.noreply.github.com>
2023-12-26 17:18:36 +0700

    Update

--- Domino/Assets/Script/Title/TitleManager.cs
+++ Domino/Assets/Script/Title/TitleManager.cs
@@ -17,7 +17,7 @@ public class TitleManager : SingletonMonoBehaviour<TitleManager>
 
     public SlotHolder hold;
 
-    [SerializeField] Sprite[] _spriteValue;
+    public Sprite[] _spriteValue;
 
     private void Start()
     {
