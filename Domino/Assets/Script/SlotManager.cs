de7b6690602f5527e12956aaecf981f86dcf349b
huydung2002 <77628916+huydung2002@users.noreply.github.com>
2024-01-03 10:56:59 +0700

    Update SlotManager.cs

--- Domino/Assets/Script/SlotManager.cs
+++ Domino/Assets/Script/SlotManager.cs
@@ -16,45 +16,23 @@ public class SlotManager : SingletonMonoBehaviour<SlotManager>
         if (_holder.titleSlot.type == Type.NGANG)
         {
             Debug.Log("Ngang");
-            _holder.RemoveDataFormList(title, false);
-            TitleManager.Instance.CheckWin();
-            foreach (var item2 in title.data.ID)
-            {
-                if (_holder.titleSlot.data.ID[0] != item2)
-                {
-                    InitSlot(item2);
-                    break;
-                }
-            }
+            TypeHorizontal(title);
             return;
         }
+
         if (_holder.titleSlot.type == Type.DOC)
         {
             Debug.Log("Doc");
-            foreach (var item in title.data.ID)
-            {
-                if (_holder.titleSlot.data.ID[0] == item)
-                {
-                    _holder.RemoveDataFormList(title, false);
-                    TitleManager.Instance.CheckWin();
-                    foreach (var item2 in title.data.ID)
-                    {
-                        if (_holder.titleSlot.data.ID[0] != item2)
-                        {
-                            InitSlot(item2);
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
+            TypeVertical(title);
         }
     }
 
     public void InitSlot(int id)//data slot
     {
+        Debug.LogError(id);
         _holder.InitTitleDataToSlot(itemTitle);
         _holder.titleSlot.data.ID[0] = id;
+        _holder.titleSlot.data.ID[1] = id;
 
         _holder.titleSlot.InitTitleData(_holder.titleSlot.data, TitleManager.Instance._spriteValue);
     }
@@ -68,5 +46,71 @@ public class SlotManager : SingletonMonoBehaviour<SlotManager>
         _holder.titleSlot.InitTitleData(_holder.titleSlot.data, TitleManager.Instance._spriteValue);
 
         _bonusTitle.OnclickNewTitle(itemTitle);
+    }
+
+    private void TypeHorizontal(ItemTitle title)
+    {
+        if (title.data.ID[0] == _holder.titleSlot.data.ID[0] && title.data.ID[1] == _holder.titleSlot.data.ID[1])
+        {
+            title.type = Type.NGANG;
+            _holder.RemoveDataFormList(itemTitle, true);
+            TitleManager.Instance.CheckWin();
+            InitSlot(title.data.ID[0]);
+            Debug.Log(3);
+            return;
+        }
+        Debug.LogWarning($"{title.data.ID[0] == _holder.titleSlot.data.ID[0]}-----1{title.data.ID[0] == _holder.titleSlot.data.ID[1]}");
+        if (title.data.ID[0] == _holder.titleSlot.data.ID[0] || title.data.ID[0] == _holder.titleSlot.data.ID[1])
+        {
+            RemoveTitle(title);
+            InitSlot(title.data.ID[1]);
+            Debug.Log(1);
+            return;
+        }
+        Debug.LogWarning($"{title.data.ID[1] == _holder.titleSlot.data.ID[0]}-----2{title.data.ID[1] == _holder.titleSlot.data.ID[1]}");
+
+        if (title.data.ID[1] == _holder.titleSlot.data.ID[0] || title.data.ID[1] == _holder.titleSlot.data.ID[1])
+        {
+            RemoveTitle(title);
+            InitSlot(title.data.ID[0]);
+            Debug.Log(2);
+        }
+    }
+
+    #region Type Dọc
+    private void TypeVertical(ItemTitle title)
+    {
+        if (title.data.ID[0] == _holder.titleSlot.data.ID[0] && title.data.ID[1] == _holder.titleSlot.data.ID[1])
+        {
+            title.type = Type.NGANG;
+            _holder.RemoveDataFormList(itemTitle, true);
+            TitleManager.Instance.CheckWin();
+            InitSlot(title.data.ID[0]);
+            return;
+        }
+
+        foreach (var item in title.data.ID)
+        {
+            if (_holder.titleSlot.data.ID[0] == item)
+            {
+                RemoveTitle(title);
+                foreach (var item2 in title.data.ID)
+                {
+                    if (_holder.titleSlot.data.ID[0] != item2)
+                    {
+                        InitSlot(item2);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    #endregion
+
+    private void RemoveTitle(ItemTitle title)
+    {
+        _holder.RemoveDataFormList(title, false);
+        TitleManager.Instance.CheckWin();
     }
 }
